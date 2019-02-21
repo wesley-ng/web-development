e6ef1f1a294c290e76d4dc32b8f59fee3c8ca779
wesley-ng <wesley.xerver@gmail.com>
2019-02-21 22:47:19 +0700

    Learning Handlebar js

---
+++ Handlebars js/vanilla choco ice/public/main.js
@@ -0,0 +1,13 @@
+const source = document.getElementById('ice-cream').innerHTML;
+
+const template = Handlebars.compile(source);
+
+const context = {
+    flavor: 'vanilla'
+};
+
+context.flavor = 'chocolate';
+const compiledHtml = template(context);
+
+const iceCreamText = document.getElementById('scream');
+iceCreamText.innerHTML = compiledHtml;
