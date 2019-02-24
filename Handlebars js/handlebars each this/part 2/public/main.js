d6d715b1ea3a196552a08976064f54d206e377b5
wesley-ng <wesley.xerver@gmail.com>
2019-02-24 17:50:42 +0700

    Learning more handlebars js

---
+++ Handlebars js/handlebars each this/part 2/public/main.js
@@ -0,0 +1,16 @@
+// * Boilerplate
+const source = document.getElementById('languagesTemp').innerHTML;
+
+const template = Handlebars.compile(source);
+
+const context = {
+    languages: [
+        {name: 'HTML'},
+        {name: 'CSS'},
+        {name: 'JavaScript'}
+    ]
+};
+
+const compiledHtml = template(context);
+const displayGoals = document.getElementById('goals');
+displayGoals.innerHTML = compiledHtml;
